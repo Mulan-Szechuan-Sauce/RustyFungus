5ad5a42b73b5fc97afe67381fbfbd45d3aabf279
Elijah Mirecki <elimirks@gmail.com>
2020-01-20 14:57:52 -0500

    Add cargo file and implement some instructions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use std::env;
 use std::fs;
 use std::fmt;
+use rand::{
+    distributions::{Distribution, Standard},
+    Rng,
+};
 
 #[derive(Copy, Clone)]
 enum Token {
@@ -14,9 +18,29 @@ enum Token {
     Int(u8),
 }
 
+#[derive(Copy, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+impl Distribution<Direction> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Direction {
+        match rng.gen_range(0, 4) {
+            0 => Direction::Up,
+            1 => Direction::Down,
+            2 => Direction::Left,
+            _ => Direction::Right,
+        }
+    }
+}
+
 struct Program {
     xptr: usize,
     yptr: usize,
+    direction: Direction,
     grid: Vec<Vec<Token>>,
 }
 
@@ -56,6 +80,16 @@ fn set_token(program: &mut Program, x: usize, y: usize, token: Token) {
     program.grid[y][x] = token;
 }
 
+fn get_token(program: &Program, x: usize, y: usize) -> Token {
+    match program.grid.get(y) {
+        Some(row) => match row.get(x) {
+            Some(token) => token.clone(),
+            None => Token::Noop,
+        },
+        None => Token::Noop,
+    }
+}
+
 fn load_program(filename: String) -> Program {
     let file_contents = fs::read_to_string(filename)
         .expect("Couldn't read file");
@@ -63,6 +97,7 @@ fn load_program(filename: String) -> Program {
     let mut program = Program {
         xptr: 0,
         yptr: 0,
+        direction: Direction::Right,
         grid: vec![],
     };
 
@@ -97,15 +132,26 @@ fn load_program(filename: String) -> Program {
 /**
  * Returns false when the program should end
  */
-fn step_program(program: & Program) -> bool {
-    // TODO:
-    return false;
+fn step_program(program: &mut Program) -> bool {
+    match get_token(program, program.xptr, program.yptr) {
+        Token::Noop       => {}, // Do nothing
+        Token::Up         => program.direction = Direction::Up,
+        Token::Down       => program.direction = Direction::Down,
+        Token::Right      => program.direction = Direction::Right,
+        Token::Left       => program.direction = Direction::Left,
+        Token::Random     => program.direction = rand::random(),
+        // FIXME:
+        Token::PrintInt => {},
+        Token::Int(_)   => {},
+    }
+
+    return true;
 }
 
-fn run_program(program: Program) {
-    println!("Program: \n{}", program);
+fn run_program(mut program: &mut Program) {
+    println!("Program: \n{}", &program);
 
-    while step_program(&program) {
+    while step_program(&mut program) {
     }
 }
 
@@ -114,8 +160,8 @@ fn main() {
 
     if args.len() == 2 {
         let filename = args.get(1).unwrap().to_string();
-        let program = load_program(filename);
-        run_program(program);
+        let mut program = load_program(filename);
+        run_program(&mut program);
     } else {
         panic!("Usage: rustyfunges <filename>")
     }
